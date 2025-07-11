0a6001e9c4a4bded359b03a80883bf827c8ad3bb
bruhmomen69 <>
2025-07-11 12:31:42 +1200

    yey

---
+++ PaperMC/src/main/kotlin/lol/mcplugs/minimessagechatplugin/paper/PaperMC.kt
@@ -0,0 +1,14 @@
+package lol.mcplugs.minimessagechatplugin.paper
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PaperMC : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package lol.mcplugs.app
+
+import lol.mcplugs.utils.Printer
+
+// This is the main entry point of the application.
+// It uses the `Printer` class from the `:utils` subproject.
+fun main() {
+    val message = "Hello JetBrains!"
+    val printer = Printer(message)
+    printer.printMessage()
+}
