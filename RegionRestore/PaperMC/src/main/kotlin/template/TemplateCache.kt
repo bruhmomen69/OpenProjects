f6470f36e6ae066b0fdd52d6abf24fd050ef07d4
bruhmomen69 <cock@balls>
2025-12-31 15:40:44 +1300

    add null-safety check to TemplateCache eviction callback to prevent releasing pinned templates when they exist in pinnedCache

--- RegionRestore/PaperMC/src/main/kotlin/template/TemplateCache.kt
+++ RegionRestore/PaperMC/src/main/kotlin/template/TemplateCache.kt
@@ -87,7 +87,7 @@ class TemplateCache(
         strategy = KacheStrategy.LRU
         expireAfterAccessDuration = ttlMinutes.minutes
         onEntryRemoved = { evicted, key, old, new ->
-            old.template.release()
+            if (pinnedCache.get(key) !== old) old.template.release()
         }
     }
 
