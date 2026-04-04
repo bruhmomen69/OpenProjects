1b350b1427ff452743348c16e372440cc0ff2db9
bruhmomen69 <cock@balls>
2026-04-04 14:37:37 +1300

    fix reobfjar being annoying with 26.1 beta builds

--- RegionRestore/nms/PaperMC-26_1/build.gradle.kts
+++ RegionRestore/nms/PaperMC-26_1/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar.Companion.shadowJar
+import io.papermc.paperweight.userdev.ReobfArtifactConfiguration
+
 plugins {
     kotlin("jvm")
     alias(libs.plugins.paperweightUserdev)
@@ -5,11 +8,25 @@ plugins {
 
 dependencies {
     implementation(project(":RegionRestore:PaperMC"))
-    paperweight.paperDevBundle("26.1.1.build.10-alpha")
+    paperweight.paperDevBundle("26.1.1.build.14-alpha")
     implementation(libs.kache)
     compileOnly(libs.nettyBuffer)
 }
 
 kotlin {
     jvmToolchain(25)
+}
+
+paperweight {
+    reobfArtifactConfiguration.set(ReobfArtifactConfiguration.MOJANG_PRODUCTION)
+}
+
+tasks.reobfJar.configure {
+    fromNamespace.set("mojang")
+    toNamespace.set("mojang")
+
+    val myFile = File.createTempFile("mappings", ".tiny")
+    myFile.deleteOnExit()
+    mappingsFile.set(myFile)
+    enabled = true
 }
